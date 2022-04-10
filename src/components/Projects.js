8bbc6520dfe9913fe6cf48c439106ee02dbece14
May Wu <maychangwu@gmail.com>
2022-04-10 17:04:20 -0400

    skills added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,41 +5,45 @@ import { Container, Row, Col } from 'react-grid-system';
 function Projects() {
   return (
     <section className='projects-container'>
-      <div className='project-img-container'>
-        <div className='project'>
-          <img
-            src='/img/git-status.png'
-            alt='portfolio'
-            className='project-img'
-          />
+      <img src='/img/dots-cyan.png' alt='dots' />
+      <h1 className='project-title'>Projects</h1>
+      <div className='projects'>
+        <div className='project-img-container'>
+          <div className='project'>
+            <img
+              src='/img/git-status.png'
+              alt='portfolio'
+              className='project-img'
+            />
+          </div>
+          <h2 className='project-subtitle'>git status</h2>
+          <div className='project-link'>
+            <a
+              href='http://my-git-status.herokuapp.com/'
+              className='project-link-name'
+            >
+              View details
+            </a>
+          </div>
         </div>
-        <h2 className='project-title'>git status</h2>
-        <div className='project-link'>
-          <a
-            href='http://my-git-status.herokuapp.com/'
-            className='project-link-name'
-          >
-            View details
-          </a>
-        </div>
-      </div>
 
-      <div className='project-img-container'>
-        <div className='project'>
-          <img
-            src='/img/reverie.png'
-            alt='portfolio'
-            className='project-img'
-          />
-        </div>
-        <h2 className='project-title'>reverie resort</h2>
-        <div className='project-link'>
-          <a
-            href='https://reverie-resort.herokuapp.com/'
-            className='project-link-name'
-          >
-            View details
-          </a>
+        <div className='project-img-container'>
+          <div className='project'>
+            <img
+              src='/img/reverie.png'
+              alt='portfolio'
+              className='project-img'
+            />
+          </div>
+          <h2 className='project-subtitle'>reverie resort</h2>
+          <div className='project-link'>
+            <a
+              href='https://reverie-resort.herokuapp.com/'
+              className='project-link-name'
+            >
+              View details
+            </a>
+          </div>
         </div>
       </div>
     </section>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,35 +4,69 @@ import '../css/Skills.css';
 function Skills() {
   return (
     <section className='skills-container'>
-       <h1 className="skills-title">Skills</h1>
- <div>
-     <div>
-         <h2 className="skills-subtitle">Development</h2>
-         <span className="skills-name">HTML</span>
-         <span className="skills-name">CSS</span>
-         <span className="skills-name">Javascript</span>
-         <span className="skills-name">React</span>
-         <span className="skills-name">Redux</span>
-         <span className="skills-name">Express.js</span>
-         <span className="skills-name">Node.js</span>
-         <span className="skills-name">Bootstrap</span>
-         <h2 className="skills-subtitle">Database</h2>
-         <span className="skills-name">PostgreSQL</span>
+      <h1 className='skills-title'>Technologies & Skills</h1>
+      <div>
+        <div className='skills-img-container'>
+          <img
+            src='/img/icons/javascript.png'
+            alt='JavaScript'
+            className='skill-icon'
+          />
+          <img src='/img/icons/html.png' alt='HTML' className='skill-icon' />
+          <img src='/img/icons/css.png' alt='CSS' className='skill-icon' />
+          <img src='/img/icons/node.png' alt='Node.js' className='skill-icon' />
+          <img
+            src='/img/icons/express.png'
+            alt='Express.js'
+            className='skill-icon'
+          />
+          <img src='/img/icons/redux.png' alt='Redux' className='skill-icon' />
+          <img src='/img/icons/react.png' alt='React' className='skill-icon' />
+          <img
+            src='/img/icons/postgresql.png'
+            alt='PostgreSQL'
+            className='skill-icon'
+          />
 
-     </div>
-     <div>
-         <h2 className="skills-subtitle">Tools</h2>
-         <span className="skills-name">Git</span>
-         <span className="skills-name">Heroku</span>
-         <span className="skills-name">Firebase</span>
-         <span className="skills-name">Chart.js</span>
-        
-         <h2 className="skills-subtitle">Design</h2>
-         <span className="skills-name">Figma</span>
-         <span className="skills-name">Adobe XD</span>
-         <span className="skills-name">Photoshop</span>
-     </div>
- </div>
+          {/* <span className="skills-name">HTML</span> */}
+          {/* <span className="skills-name">CSS</span> */}
+          {/* <span className="skills-name">Javascript</span> */}
+          {/* <span className="skills-name">React</span> */}
+          {/* <span className="skills-name">Redux</span> */}
+          {/* <span className="skills-name">Express.js</span> */}
+          {/* <span className="skills-name">Node.js</span> */}
+          {/* <span className="skills-name">Axios</span> */}
+          {/* <span className="skills-name">RESTful APIs</span> */}
+          {/* <span className="skills-name">Git</span> */}
+          {/* <span className="skills-name">Bootstrap</span> */}
+          {/* <h2 className="skills-subtitle">Database</h2> */}
+          {/* <span className="skills-name">PostgreSQL</span> */}
+        </div>
+        <h2 className='skills-subtitle'>Tools</h2>
+        <div className='skills-img-container'>
+          <img src='/img/icons/git.png' alt='Git' className='skill-icon' />
+          <img
+            src='/img/icons/heroku.png'
+            alt='Heroku'
+            className='skill-icon'
+          />
+          <img
+            src='/img/icons/firebase.png'
+            alt='Firebase'
+            className='skill-icon'
+          />
+        </div>
+
+        <h2 className='skills-subtitle'>UI Design</h2>
+        <div className='skills-img-container'>
+          <img src='/img/icons/figma.png' alt='Figma' className='skill-icon' />
+          <img
+            src='/img/icons/photoshop.png'
+            alt='Photoshop'
+            className='skill-icon'
+          />
+        </div>
+      </div>
     </section>
   );
 }

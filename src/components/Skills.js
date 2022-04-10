d7b6e68d9eb51116e19e57fe359c71601534d3bd
May Wu <maychangwu@gmail.com>
2022-04-10 13:27:25 -0400

    skills component updateD

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,22 +4,35 @@ import '../css/Skills.css';
 function Skills() {
   return (
     <section className='skills-container'>
-      <div className='skills-title'>
-        <h2>Skills</h2>
-      </div>
-      <div className='skills-list'>
-        <h3>Development</h3>
-        <ul>
-          <li>JavaScript</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>React</li>
-          <li>Express.js</li>
-          <li>Node.js</li>
-          <li>Bootstrap</li>
-          <li>Redux</li>
-        </ul>
-      </div>
+       <h1 className="skills-title">Skills</h1>
+ <div>
+     <div>
+         <h2 className="skills-subtitle">Development</h2>
+         <span className="skills-name">HTML</span>
+         <span className="skills-name">CSS</span>
+         <span className="skills-name">Javascript</span>
+         <span className="skills-name">React</span>
+         <span className="skills-name">Redux</span>
+         <span className="skills-name">Express.js</span>
+         <span className="skills-name">Node.js</span>
+         <span className="skills-name">Bootstrap</span>
+         <h2 className="skills-subtitle">Database</h2>
+         <span className="skills-name">PostgreSQL</span>
+
+     </div>
+     <div>
+         <h2 className="skills-subtitle">Tools</h2>
+         <span className="skills-name">Git</span>
+         <span className="skills-name">Heroku</span>
+         <span className="skills-name">Firebase</span>
+         <span className="skills-name">Chart.js</span>
+        
+         <h2 className="skills-subtitle">Design</h2>
+         <span className="skills-name">Figma</span>
+         <span className="skills-name">Adobe XD</span>
+         <span className="skills-name">Photoshop</span>
+     </div>
+ </div>
     </section>
   );
 }

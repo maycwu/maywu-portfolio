fdcf924a32de6b6360f5f8b817abe1df45f3370e
May Wu <maychangwu@gmail.com>
2022-04-11 02:49:49 -0400

    styling changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,7 +12,7 @@ function NavBar() {
         <ul className='navbar-ul'>
           <li><a href="#home">home</a> </li>
           <li><a href="#about">about</a> </li>
-          <li><a href="#projects">project</a> </li>
+          <li><a href="#projects">projects</a> </li>
           <li><a href="#contact">contact</a> </li>
         </ul>
       </div>

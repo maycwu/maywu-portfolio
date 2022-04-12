ab09bda479828c6271e696b157b1c4f8ff09b7b3
May Wu <maychangwu@gmail.com>
2022-04-12 14:28:32 -0400

    added animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,41 @@
 import React from 'react';
 import '../css/Home.css';
 import Particle from './Particle';
-
+import { motion } from 'framer-motion';
 
 function Home() {
   return (
     <section className='home-container' id='home'>
-    <Particle/>
-      <div className="home-sub-container">
-      <div className='home'>
-      <div className='home-text'>
-            Hi there! <span> Im May </span>
-            <br />
-            Full Stack
-            <br />
-            Software Engineer
+      <Particle />
+        <div className='home-sub-container'>
+          <div className='home'>
+            <div className='home-text'>
+              Hi there! <span> Im May </span>
+              <br />
+              Full Stack
+              <br />
+              Software Engineer
+            </div>
           </div>
-      </div> 
-
-      </div>
+        </div>
+      <motion.div
+        initial='visible'
+        whileInView='hidden'
+        viewport={{ once: true }}
+        transition={{ duration: 1 }}
+        variants={{
+          visible: { opacity: 1, scale: 1 },
+          hidden: { opacity: 0, scale: 0 },
+        }}
+      >
+        <div className='learn-more-container'>
+          <div className='learn-more'>
+            <div className='learn-more-text'>
+               Scroll down to learn more!
+            </div>
+          </div>
+        </div>
+      </motion.div>
     </section>
   );
 }
